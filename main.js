0f45c89faf30923c2b93a550981661bd9581acd6
testproject-testing-cf <56145975+zyrill707@users.noreply.github.com>
2024-04-19 09:31:53 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-console.log('Hello World!');
+window.addEventListener('scroll', function() {
+  var scrollToTopBtn = document.getElementById('scrollToTopBtn');
+  if (window.scrollY > 300) { 
+    scrollToTopBtn.style.display = 'block';
+  } else {
+    scrollToTopBtn.style.display = 'none';
+  }
+});
+
+document.getElementById('scrollToTopBtn').addEventListener('click', function() {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
+});

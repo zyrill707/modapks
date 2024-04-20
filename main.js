f062881c5ae496c963756bcd1a8a0716ac784e49
testproject-testing-cf <56145975+zyrill707@users.noreply.github.com>
2024-04-20 08:21:08 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,37 @@
+function trackPageView() {
+  gtag('config', 'G-9VH7RW3Y1X');
+}
+
+window.addEventListener('load', trackPageView);
+
+function fetchUserCount() {}
+
+function updateUserCount(newCount) {
+  const userCountElement = document.getElementById('userCount');
+  const currentCount = parseInt(userCountElement.textContent);
+  animateCountChange(userCountElement, currentCount, newCount);
+  userCountElement.textContent = newCount;
+}
+
+function animateCountChange(element, currentCount, newCount) {
+  const diff = newCount - currentCount;
+  const duration = 1000;
+  const fps = 60;
+  const increment = diff / (duration / (1000 / fps));
+  let count = currentCount;
+  const interval = setInterval(() => {
+    count += increment;
+    if ((increment > 0 && count >= newCount) || (increment < 0 && count <= newCount)) {
+      clearInterval(interval);
+      count = newCount;
+    }
+    element.textContent = Math.round(count);
+  }, 1000 / fps);
+}
+
 window.addEventListener('scroll', function() {
   var scrollToTopBtn = document.getElementById('scrollToTopBtn');
-  if (window.scrollY > 300) { 
+  if (window.scrollY > 300) {
     scrollToTopBtn.style.display = 'block';
   } else {
     scrollToTopBtn.style.display = 'none';

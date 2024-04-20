3a8cd31b6226011138f8d355c508a30bf220af1b
testproject-testing-cf <56145975+zyrill707@users.noreply.github.com>
2024-04-20 13:44:10 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+let userCount = 2000; // Starting user count
+let increment = 10; // Increment value
+let formattedCount = formatNumber(userCount);
+
 function trackPageView() {
   // Replace 'G-9VH7RW3Y1X' with your actual Measurement ID
   gtag('config', 'G-9VH7RW3Y1X');
@@ -5,24 +9,41 @@ function trackPageView() {
 
 window.addEventListener('load', function() {
   trackPageView();
-  updateUserCountWithAnimation(0, 9000); // Start the animation from 0 to 9000
+  updateUserCountWithAnimation(0, userCount); // Start the animation from 0 to starting count
+  setInterval(increaseUserCount, 3600000); // Increase user count every hour (3600000 milliseconds)
 });
+
+function increaseUserCount() {
+  userCount += increment;
+  formattedCount = formatNumber(userCount);
+  updateUserCountWithAnimation(userCount - increment, userCount);
+}
 
 function updateUserCountWithAnimation(startCount, endCount) {
   const userCountElement = document.getElementById('userCount');
   let count = startCount;
   const fps = 60;
   const duration = 2000; // Animation duration in milliseconds
-  const increment = (endCount - startCount) / (duration / (1000 / fps));
+  const incrementValue = (endCount - startCount) / (duration / (1000 / fps));
 
   const interval = setInterval(() => {
-    count += increment;
+    count += incrementValue;
     if (count >= endCount) {
       clearInterval(interval);
       count = endCount;
     }
     userCountElement.textContent = Math.round(count);
   }, 1000 / fps);
+}
+
+function formatNumber(number) {
+  const symbols = ['', 'K', 'M', 'B', 'T'];
+  let tier = Math.log10(number) / 3 | 0;
+  if (tier == 0) return number;
+  const suffix = symbols[tier];
+  const scale = Math.pow(10, tier * 3);
+  const scaled = number / scale;
+  return scaled.toFixed(1) + suffix;
 }
 
 window.addEventListener('scroll', function() {
